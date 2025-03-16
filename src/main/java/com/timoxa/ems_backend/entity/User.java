3bb6313bfa0528ab2d473cca68070a6a02e19f32
Timur Rakhmatullaev <timurrahmatullaev703@gmail.com>
2025-03-16 14:50:30 +0600

    add User entity

--- src/main/java/com/timoxa/ems_backend/entity/User.java
+++ src/main/java/com/timoxa/ems_backend/entity/User.java
@@ -11,25 +11,29 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "employees")
-public class Employee {
+@Table(name = "users")
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "first_name")
+    @Column(name = "username")
     private String firstName;
 
-    @Column(name = "last_name")
-    private String lastName;
+    @Column(name = "password")
+    private String password;
 
-    @Column(name = "email_id", nullable = false, unique = true)
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
 
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
 }
