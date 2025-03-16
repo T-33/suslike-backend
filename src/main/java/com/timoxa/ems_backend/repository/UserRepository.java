08a2fea1a94608ebe15b6bee66b1754a4233fc0e
Timur Rakhmatullaev <timurrahmatullaev703@gmail.com>
2025-03-16 14:43:01 +0600

    add user repository

--- src/main/java/com/timoxa/ems_backend/repository/UserRepository.java
+++ src/main/java/com/timoxa/ems_backend/repository/UserRepository.java
@@ -3,6 +3,6 @@ package com.timoxa.ems_backend.repository;
 import com.timoxa.ems_backend.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+public interface UserRepository extends JpaRepository<Employee, Long> {
 
 }
--- src/main/java/com/timoxa/ems_backend/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/timoxa/ems_backend/service/impl/EmployeeServiceImpl.java
@@ -4,7 +4,7 @@ import com.timoxa.ems_backend.dto.EmployeeDto;
 import com.timoxa.ems_backend.entity.Employee;
 import com.timoxa.ems_backend.exception.ResourceNotFoundException;
 import com.timoxa.ems_backend.mapper.EmployeeMapper;
-import com.timoxa.ems_backend.repository.EmployeeRepository;
+import com.timoxa.ems_backend.repository.UserRepository;
 import com.timoxa.ems_backend.service.EmployeeService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,20 +15,20 @@ import java.util.List;
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
 
-    private EmployeeRepository employeeRepository;
+    private UserRepository userRepository;
 
     @Override
     public EmployeeDto createEmployee(EmployeeDto employeeDto) {
 
         Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
-        Employee savedEmployee = employeeRepository.save(employee);
+        Employee savedEmployee = userRepository.save(employee);
 
         return EmployeeMapper.mapToEmployeeDto(savedEmployee);
     }
 
     @Override
     public EmployeeDto getEmployeeById(Long employeeId) {
-        Employee employee = employeeRepository.findById(employeeId)
+        Employee employee = userRepository.findById(employeeId)
                 .orElseThrow(() -> new ResourceNotFoundException("Employee with a given id does not exist " + employeeId));
 
         return EmployeeMapper.mapToEmployeeDto(employee);
@@ -36,31 +36,31 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public List<EmployeeDto> getAllEmployees() {
-        List<Employee> employees = employeeRepository.findAll();
+        List<Employee> employees = userRepository.findAll();
 
         return employees.stream().map(EmployeeMapper::mapToEmployeeDto).toList();
     }
 
     @Override
     public EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee) {
-        Employee employee = employeeRepository.findById(employeeId)
+        Employee employee = userRepository.findById(employeeId)
                 .orElseThrow(() -> new ResourceNotFoundException("Employee with a given id does not exist " + employeeId));
 
         employee.setFirstName(updatedEmployee.getFirstName());
         employee.setLastName(updatedEmployee.getLastName());
         employee.setEmail(updatedEmployee.getEmail());
 
-        employeeRepository.save(employee);
+        userRepository.save(employee);
 
         return EmployeeMapper.mapToEmployeeDto(employee);
     }
 
     @Override
     public EmployeeDto deleteEmployee(Long employeeId) {
-        Employee employee = employeeRepository.findById(employeeId)
+        Employee employee = userRepository.findById(employeeId)
                 .orElseThrow(() -> new ResourceNotFoundException("Employee with a given id does not exist " + employeeId));
 
-        employeeRepository.delete(employee);
+        userRepository.delete(employee);
 
         return EmployeeMapper.mapToEmployeeDto(employee);
     }
